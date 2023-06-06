a9dc11fc97776f2032f8bab0e1818b5def487e5c
Spencer Longhurst <spencersean12@gmail.com>
2023-06-05 21:04:36 -0600

    Started the header

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styling/App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='top'>
+      <Header/>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styling/Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='innerHeader'>
+      <h2>FORECASTING</h2>
+      <p className='innerLine'/>
+      <h2>CONTACT</h2>
+      {/* <p>____________________________________________________</p> */}
+      </div>
+    </div>
+  )
+}
+
+export default Header
